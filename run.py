2fd5060b1f54adee9e2e9c226461042be679edb7
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-05-08 09:16:24 +0000

    added initial functions and notes explaining what they will do

--- run.py
+++ run.py
@@ -1,10 +1,51 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 def test():
+    """
+    Test to check if it gets printed to the console 
+    when deployed
+    (it does)
+    """
     print("Testing....\n")
     print("checking if i need to refresh...\n")
     print("Connected!")
 
 test()
+
+# Creating functions we need:
+
+def print_rules():
+    """
+    Prints rules of game to terminal 
+    "Do you want to proceed Y/N" Text input to check
+    if user wants to continue 
+    """
+
+def name_input():
+    """
+    Takes name of the user and asks what size grid they want?
+    6/10 grid size 
+    """
+
+def create_grid():
+    """
+    creates player grid 
+    creates computer grid 
+    prints grids to terminal
+    """
+
+def player_turn():
+    """
+    Allows player to take their turn
+    Validates their guess, Hit or Miss.
+    """
+
+def computer_turn():
+    """
+    Generates computer turn
+    Validates the guess, Hit or Miss
+    """
+
+def play():
+    """
+    function to start game
+    """
